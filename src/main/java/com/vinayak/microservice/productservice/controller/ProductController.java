386179c04f42ca331365f7a79c7ad4ad4adcd438
Vinayak <vinayakchaurasia99@gmail.com>
2021-05-31 01:04:30 +0530

    Added mappings for creating and finding products

--- src/main/java/com/vinayak/microservice/productservice/controller/ProductController.java
+++ src/main/java/com/vinayak/microservice/productservice/controller/ProductController.java
@@ -1,4 +1,32 @@
 package com.vinayak.microservice.productservice.controller;
 
+import com.vinayak.microservice.productservice.model.Product;
+import com.vinayak.microservice.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Product> findAll(){
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody Product product){
+        productRepository.save(product);
+    }
+
+
 }
+
